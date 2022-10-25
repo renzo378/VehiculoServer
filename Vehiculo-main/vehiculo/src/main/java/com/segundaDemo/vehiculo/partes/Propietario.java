17ef2c3ea88ec76a3323a84bc31f7f2722a0c951
renzo378 <69973902+renzo378@users.noreply.github.com>
2022-10-25 15:22:32 -0300

    Update Propietario.java

--- Vehiculo-main/vehiculo/src/main/java/com/segundaDemo/vehiculo/partes/Propietario.java
+++ Vehiculo-main/vehiculo/src/main/java/com/segundaDemo/vehiculo/partes/Propietario.java
@@ -3,7 +3,6 @@ package com.segundaDemo.vehiculo.partes;
 import lombok.Data;
 
 @Data
-@Entity
 public class Propietario extends Persona{
     private String documento;
     static final String  Saludo = "Hola";
